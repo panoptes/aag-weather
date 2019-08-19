1cb8a961b0e69232468c36b4dad6c07cc8e9f95b
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-08-20 07:34:53 +1000

    Initial commit of files. Mostly a transfer from https://github.com/panoptes/POCS where this has been working for a while.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aag-weather
+
+A small script that is capable of reading the [Lunatico AAG Cloud Watcher](https://www.lunatico.es/ourproducts/aag-cloud-watcher.html) weather station data, including the anemometer.
---
+++ setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# Licensed under an MIT style license - see LICENSE.txt
+
+from setuptools import setup, find_namespace_packages
+
+from configparser import ConfigParser
+from distutils.command.build_py import build_py
+
+from aag.version import __version__
+
+# Get some values from the setup.cfg
+conf = ConfigParser()
+conf.read(['setup.cfg'])
+metadata = dict(conf.items('metadata'))
+
+AUTHOR = metadata.get('author', '')
+AUTHOR_EMAIL = metadata.get('author_email', '')
+DESCRIPTION = metadata.get('description', '')
+KEYWORDS = metadata.get('keywords', 'Lunatico AAG weather station')
+LICENSE = metadata.get('license', 'unknown')
+LONG_DESCRIPTION = metadata.get('long_description', '')
+NAME = metadata.get('name', 'aag-weather')
+PACKAGENAME = metadata.get('package_name', 'packagename')
+URL = metadata.get('url', 'https://projectpanoptes.org')
+
+requirements = list()
+requirements_fn = 'requirements.txt'
+with open(requirements_fn) as f:
+    requirements = f.read().splitlines()
+
+modules = {
+    'required': requirements,
+    'testing': [
+        'codecov',
+        'coverage',
+        'coveralls',
+        'mocket',
+        'pycodestyle==2.3.1',
+        'pytest>=3.6',
+        'pytest-cov',
+    ],
+}
+
+
+setup(name=NAME,
+      version=__version__,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      license=LICENSE,
+      url=URL,
+      keywords=KEYWORDS,
+      python_requires='>=3.6',
+      setup_requires=['pytest-runner'],
+      tests_require=modules['testing'],
+      # List additional groups of dependencies here (e.g. development
+      # dependencies). You can install these using the following syntax,
+      # for example:
+      # $ pip install -e .[dev,test]
+      install_requires=modules['required'],
+      packages=find_namespace_packages(exclude=['tests', 'test_*']),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: POSIX',
+          'Programming Language :: C',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          'Topic :: Scientific/Engineering :: Physics',
+      ],
+      cmdclass={'build_py': build_py}
+      )

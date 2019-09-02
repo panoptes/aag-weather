790cf0ab821191aee77fb96be0deb730694789fe
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-09-03 09:47:34 +1000

    Script param consistency

--- scripts/read-aag.py
+++ scripts/read-aag.py
@@ -26,13 +26,13 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description="Read an AAG CloudWatcher")
-    parser.add_argument('--config_file', required=True,
+    parser.add_argument('--config-file', required=True,
                         help='Config file that contains the AAG params.')
     parser.add_argument('--store-result', default=False, action='store_true',
                         help='If data entries should be saved to db, default False.')
     parser.add_argument('--db-file', default='weather.db', help='Name of sqlite3 db file to use.')
     parser.add_argument('--db-table', default='weather', help='Name of db table to use.')
-    parser.add_argument('--serial_address', default=None,
+    parser.add_argument('--serial-address', default=None,
                         help='USB serial address to use. If None, value from config will be used.')
     parser.add_argument('--verbose', action='store_true', default=False, help='Verbose.')
 

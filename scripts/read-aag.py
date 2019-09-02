347aeeefcfb39fef46de9e61de2e885396bcb1ea
Wilfred Tyler Gee <wtylergee@gmail.com>
2019-09-03 09:41:26 +1000

    Add param for storing results.

--- scripts/read-aag.py
+++ scripts/read-aag.py
@@ -4,7 +4,7 @@ import time
 from weather import AAGCloudSensor
 
 
-def main(config_file=None, verbose=False, **kwargs):
+def main(config_file=None, store_result=False, verbose=False, **kwargs):
     if config_file is None:
         print('Must pass config_file')
         return
@@ -13,7 +13,7 @@ def main(config_file=None, verbose=False, **kwargs):
 
     while True:
         try:
-            data = aag.capture(store_result=True)
+            data = aag.capture(store_result=store_result)
             if verbose:
                 print(f'{data!r}')
 
@@ -28,6 +28,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Read an AAG CloudWatcher")
     parser.add_argument('--config_file', required=True,
                         help='Config file that contains the AAG params.')
+    parser.add_argument('--store-result', default=False, action='store_true',
+                        help='If data entries should be saved to db, default False.')
     parser.add_argument('--db-file', default='weather.db', help='Name of sqlite3 db file to use.')
     parser.add_argument('--db-table', default='weather', help='Name of db table to use.')
     parser.add_argument('--serial_address', default=None,

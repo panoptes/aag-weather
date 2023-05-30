dff31d6bb9595cf4e3f35e05f5685b8fe9f980ee
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-05-29 17:39:37 -1000

    * Try to test with /dev/null

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,9 +1,10 @@
-import pytest
+import os
 
 from aag.weather import CloudSensor
 
 
 def test_create_sensor():
+    os.environ['AAG_SERIAL_PORT'] = '/dev/null'
     sensor = CloudSensor(connect=False)
     assert isinstance(sensor, CloudSensor)
     assert not sensor.is_connected
